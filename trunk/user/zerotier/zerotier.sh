a65956addbaf3c2f26c8c25163bc8d69aa378efe
axiang117 <axiang117@hotmail.com>
2022-07-29 21:56:38 +0800

    zerotier

--- trunk/user/zerotier/zerotier.sh
+++ trunk/user/zerotier/zerotier.sh
@@ -43,19 +43,22 @@ start_instance() {
 	
 	if [ -n "$planet"]; then
 		logger -t "zerotier" "找到planet,正在写入文件,请稍后..."
-		echo "$planet" >$config_path/planet
+		echo "$planet" >$config_path/planet.tmp
+		base64 -d $config_path/planet.tmp >$config_path/planet
 	fi
 	
 	if [ -f "$PLANET" ]; then
-		if [ ! -s "$PLANET" ];then
-			logger -t "zerotier" "planet文件为空,删除..."
+		if [ ! -s "$PLANET" ]; then
+			logger -t "zerotier" "自定义planet文件为空,删除..."
 			rm -f $config_path/planet
+			rm -f $PLANET
 			nvram set zerotier_planet=""
 			nvram commit
 		else
-			logger -t "zerotier" "planet文件不为空,创建..."
-			planet="$(cat $PLANET)"
-			echo "$planet" >$config_path/planet
+			logger -t "zerotier" "自定义planet文件不为空,创建..."
+			planet="$(base64 $PLANET)"
+			cp -f $PLANET $config_path/planet
+			rm -f $PLANET
 			nvram set zerotier_planet="$planet"
 			nvram commit
 		fi
